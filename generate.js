4114553123234d9b379f47eeeb589d88ecd814f7
Dexter Shepherd <dexter@kadenze.com>
2017-01-01 19:50:10 -0800

    generator should update package.json name

--- generate.js
+++ generate.js
@@ -1,12 +1,24 @@
+"use strict";
 // generate a new sketch folder for today
 const exec = require('child_process').exec;
 const moment = require('moment');
+const fs = require('fs');
+
+const path = "./" + moment().format('MM-DD-YY');
 
 function generate() {
-  exec("cp -rf default " + moment().format('MM-DD-YY')  , (err, stdout, sterr) => {
+  exec("cp -rf default " + path, (err, stdout, sterr) => {
     if(err) {
       return console.log(err);
     }
+    let pack = JSON.parse(fs.readFileSync(path + "/package.json", 'utf8'));
+    pack.name = "Daily Sketch " + moment().format('MM-DD-YY');
+
+    fs.writeFile(path + "/package.json", JSON.stringify(pack, null, 2), (err) => {
+      if (err) {
+        return console.log(err);
+      }
+    })
   });
 }
 
