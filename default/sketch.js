6ef2cb4925569df5684658ccc3271e3e1739844e
Dexter Shepherd <dexter@kadenze.com>
2017-01-06 14:59:51 -0800

    updated default with palette

--- default/sketch.js
+++ default/sketch.js
@@ -1,12 +1,19 @@
 const p5 = require('p5');
 
 new p5(p => {
+  let palettes;
+
+  p.preload = function() {
+    palettes = p.loadJSON('./kuler-palette.json');
+  }
+
   p.setup = function() {
     const canvas = p.createCanvas(800, 800);
+    console.log(palettes[0]);
   }
 
   p.draw = function() {
-    p.background(100);
+    p.background(240);
   }
 });
 
