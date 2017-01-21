296e10fb179aaf3d0dc91cd986df8c45741036b2
Dexter Shepherd <dexter@kadenze.com>
2017-01-20 19:29:06 -0800

    reset default

--- default/sketch.js
+++ default/sketch.js
@@ -1,12 +1,6 @@
 const p5 = require('p5');
 
 new p5(p => {
-  let palettes, particle;
-
-  p.preload = function() {
-    palettes = p.loadJSON('./kuler-palette.json');
-  }
-
   p.setup = function() {
     const canvas = p.createCanvas(800, 800);
   }
